45bb26637d33744ed5ebe2df0f017b072229c9e7
YassineMinjon <31763552+YassineMinjon@users.noreply.github.com>
2017-09-12 20:21:10 +0200

    Add files via upload

--- HEOBP Opdracht 1.cpp
+++ HEOBP Opdracht 1.cpp
@@ -1,5 +1,5 @@
 // HEOBP Opdracht 1.cpp : Defines the entry point for the console application.
-//
+//Yassine minjon
 
 #include "stdafx.h"
 #include <iostream>
@@ -8,13 +8,14 @@
 
 using namespace std;
 
-class Lesson //The lesson class is the base to every different subject.
+class Lesson //De Lesson class is de basis van alle lessen op het rooster en bevat alle variabele die iedere les sowieso nodig heeft.
 {
 	private:
-		int month;
+		int month; 
 		int week;
 		int day;
 		int classRoom;
+		int length;
 		string time;
 		string name;
 		string teacher;
@@ -27,19 +28,22 @@ class Lesson //The lesson class is the base to every different subject.
 		int getWeek() { return week; }
 		int getDay() { return day; }
 		int getClassroom() { return classRoom; }
+		int getLength() { return length; }
 		string getTime() { return time; }
 		string getName() { return name; }
 		string getTeacher() { return teacher; }
 		string getDiscription() { return discription; }
 
 		void setDate(int monthVal, int weekVal, int dayVal) { month = monthVal; week = weekVal; day = dayVal; }
+		void setClassRoom(int classRoomVal) { classRoom = classRoomVal;}
+		void setLenght(int lengthVal) { length = lengthVal; }
 		void setTime(float timeVal) { time = timeVal; }
 		void setName(string lessonName) { name = lessonName; }
 		void setTeacher(string teacherName) { teacher = teacher; }
 		void setDiscription(string newDiscription) { discription = newDiscription; }
 
 		Lesson();
-		Lesson(int monthVal, int weekVal, int dayVal, int classRoomVal, string timeVal, string nameVal, string teacherVal, string newDiscription);
+		Lesson(int monthVal, int weekVal, int dayVal, int classRoomVal, int lengthVal, string timeVal, string nameVal, string teacherVal, string newDiscription);
 		~Lesson();
 };
 
@@ -47,12 +51,13 @@ int Lesson::numberOfLessons = 0;
 
 Lesson::Lesson(void) { numberOfLessons++; }
 
-Lesson::Lesson(int monthVal, int weekVal, int dayVal, int classRoomVal, string timeVal, string nameVal, string teacherVal, string newDiscription)
+Lesson::Lesson(int monthVal, int weekVal, int dayVal, int classRoomVal, int lengthVal, string timeVal, string nameVal, string teacherVal, string newDiscription)
 {
 	this->month = monthVal;
 	this->week = weekVal;
 	this->day = dayVal;
 	this->classRoom = classRoomVal;
+	this->length = lengthVal;
 	this->time = timeVal;
 	this->name = nameVal;
 	this->teacher = teacherVal;
@@ -63,28 +68,28 @@ Lesson::Lesson(int monthVal, int weekVal, int dayVal, int classRoomVal, string t
 Lesson::~Lesson() { numberOfLessons--; }
 
 
-class KernModule : public Lesson
+class KernModule : public Lesson // De KernModule class inherit alle functies en variabele van de Lesson class, de Kernmodule heeft alleen zelf nog een study variabele, deze word gebruikt om aan te geven voor welke studierichting dit kernvak is.
 {
 	private:
-		string study;
+		string study; 
 
 	public:
 		string getStudy() { return study; }
 
 		void setStudy(string studyVal) { study = studyVal; }
 
-		KernModule(int monthVal, int weekVal, int dayVal, int classRoomVal, string timeVal, string nameVal, string teacherVal, string newDiscription, string studyVal);
+		KernModule(int monthVal, int weekVal, int dayVal, int classRoomVal, int lengthVal, string timeVal, string nameVal, string teacherVal, string newDiscription, string studyVal);
 		KernModule() : Lesson() {};
 };
 
-KernModule::KernModule(int monthVal, int weekVal, int dayVal, int classRoomVal, string timeVal, string nameVal, string teacherVal, string newDiscription, string studyVal) 
-: Lesson(monthVal ,weekVal ,dayVal ,classRoomVal ,timeVal ,nameVal ,teacherVal ,newDiscription)
+KernModule::KernModule(int monthVal, int weekVal, int dayVal, int classRoomVal, int lengthVal, string timeVal, string nameVal, string teacherVal, string newDiscription, string studyVal)
+: Lesson(monthVal ,weekVal ,dayVal ,classRoomVal ,lengthVal ,timeVal ,nameVal ,teacherVal ,newDiscription)
 {
 	this->study = studyVal;
 }
 
 
-class KeuzeVak : public Lesson
+class KeuzeVak : public Lesson // Net als de KernModule class is de KeuzeVak class een Lesson, deze heeft alleen ook een eigen list met de namen van de studenten voor wie dit keuzevak geld.
 {
 	private:
 		list<string> students;
@@ -93,24 +98,44 @@ class KeuzeVak : public Lesson
 	public:
 		list<string> getStudents(){ return students; }
 
-		void setStudents(string studentName) { students.insert(it, studentName); }
+		void addStudents(string studentName) { students.insert(it, studentName); }
 
-		KeuzeVak(int monthVal, int weekVal, int dayVal, int classRoomVal, string timeVal, string nameVal, string teacherVal, string newDiscription, list<string> students);
+		KeuzeVak(int monthVal, int weekVal, int dayVal, int classRoomVal, int lengthVal, string timeVal, string nameVal, string teacherVal, string newDiscription, list<string> studentList);
 		KeuzeVak() : Lesson() {};
 
 };
 
-KeuzeVak::KeuzeVak(int monthVal, int weekVal, int dayVal, int classRoomVal, string timeVal, string nameVal, string teacherVal, string newDiscription, list<string> students)
-: Lesson(monthVal, weekVal, dayVal, classRoomVal, timeVal, nameVal, teacherVal, newDiscription)
+KeuzeVak::KeuzeVak(int monthVal, int weekVal, int dayVal, int classRoomVal, int lengthVal, string timeVal, string nameVal, string teacherVal, string newDiscription, list<string> studentList)
+: Lesson(monthVal, weekVal, dayVal, classRoomVal, lengthVal, timeVal, nameVal, teacherVal, newDiscription)
 {
-
+	this->students = studentList;
 }
 
 
+class Student // De Student class bevat alle gegevens die de app nodig heeft om de luiste lessen voor iedere student weer te geven.
+{
+	private:
+		int studentNumber;
+		string study;
+		string name;
 
+	public:
+		int getStudentNumber() { return studentNumber; }
+		string getStudy() { return study; }
+		string getName() { return name; }
+
+		void setStudentNumber(int newStudentNumber) { studentNumber = newStudentNumber; }
+		void setStudy(string newStudy) { study = newStudy; }
+		void setName(string newName) { name = newName; }
+
+		Student();
+};
+
+Student::Student(void){}
 
 int main()
 {
+	
     return 0;
 }
 
